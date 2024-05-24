194e9bf1a1375808f7598c6904b374f69a319f0c
Anudeepkumar7 <125281933+Anudeepkumar7@users.noreply.github.com>
2024-05-24 18:16:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 if (process.env.NODE_ENV !== "production") {
     require('dotenv').config();
 }
-
+console.log("MAPBOX_TOKEN:", process.env.MAPBOX_TOKEN);
 const express = require('express');
 const path = require('path');
 const helmet = require('helmet');
